32d8c6bf41a64d7f12a075ac1b5ba46b8e0e7055
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-03-07 16:53:44 +0100

    Added missing files

---
+++ GreenWay/src/main/java/dev/redy1908/greenway/GreenWayApplication.java
@@ -0,0 +1,13 @@
+package dev.redy1908.greenway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenWayApplication.class, args);
+    }
+
+}
---
+++ GreenWay/src/test/java/dev/redy1908/greenway/GreenWayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.redy1908.greenway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
