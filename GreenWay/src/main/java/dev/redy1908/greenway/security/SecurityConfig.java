d74b657a1dd32b178e872e5ae2b926e3111f53f0
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-04-16 15:48:14 +0200

    Updated

--- GreenWay/src/main/java/dev/redy1908/greenway/security/SecurityConfig.java
+++ GreenWay/src/main/java/dev/redy1908/greenway/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, POST_ADMIN_LIST_URL).hasRole(ADMIN_ROLE)
+                        .requestMatchers(HttpMethod.GET, GET_ADMIN_LIST_URL).hasRole(ADMIN_ROLE)
                         .requestMatchers(HttpMethod.POST, POST_ADMIN_LIST_URL).hasRole(ADMIN_ROLE)
                         .requestMatchers(HttpMethod.GET, GET_ADMIN_DELIVERY_MAN_LIST_URL).hasAnyRole(ADMIN_ROLE, DELIVERY_MAN_ROLE)
                         .requestMatchers(HttpMethod.POST, POST_DELIVERY_MAN_LIST_URL).hasRole(DELIVERY_MAN_ROLE)
