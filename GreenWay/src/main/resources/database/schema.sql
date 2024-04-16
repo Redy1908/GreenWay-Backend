b9333bb8e3f7ef2e5b6d4d8529151d6aec8c3fb4
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-04-16 16:08:26 +0200

    Updated

--- GreenWay/src/main/resources/database/schema.sql
+++ GreenWay/src/main/resources/database/schema.sql
@@ -1,47 +1,40 @@
+create table deliveries
+(
+    id                      serial primary key,
+    created_at              timestamp(6),
+    created_by              varchar(255),
+    updated_at              timestamp(6),
+    updated_by              varchar(255),
+    deposit_address         varchar(255) not null,
+    deposit_coordinates     geometry     not null,
+    estimated_delivery_date date,
+    receiver                varchar(255) not null,
+    receiver_address        varchar(255) not null,
+    receiver_coordinates    geography,
+    sender                  varchar(255) not null,
+    sender_address          varchar(255) not null,
+    weight_kg               integer      not null,
+    vehicle_id              integer      constraint fkgjj47cndyarbxrqimqu8q16n8 references vehicles,
+    deliveries_order        integer
+);
+
 create table delivery_men
 (
-    id       bigserial primary key,
-    username varchar(255) not null unique
+    id                  bigserial primary key,
+    username            varchar(255) not null constraint uk_mdmis23uvd7ma04egeq65i3cw unique,
+    delivery_vehicle_id integer constraint uk_hm2urbq6fuxymhkct0qj7hcef unique constraint fkbre6fb6j0p867w6k7w46wo539 references vehicles
 );
 
 create table vehicles
 (
-    id              bigserial primary key,
-    model_name      varchar(255)     not null,
-    max_autonomy_km double precision not null,
-    max_capacity_kg double precision not null,
-    created_at      timestamp(6),
-    updated_at      timestamp(6),
-    created_by      varchar(255),
-    updated_by      varchar(255)
-);
-
-create table deliveries
-(
-    id                      bigserial primary key,
-    deposit_address         varchar(255) not null,
-    deposit_coordinates     geometry     not null,
-    delivery_man_id         bigint constraint fkjubqdj7mnhpndfgdrjlyns8by references delivery_men,
-    vehicle_id              bigint constraint fkgjj47cndyarbxrqimqu8q16n8 references vehicles,
-    estimated_delivery_date date         not null,
-    created_at              timestamp(6),
-    updated_at              timestamp(6),
-    created_by              varchar(255),
-    updated_by              varchar(255)
-);
-
-create table delivery_packages
-(
-    id                   bigserial primary key,
-    sender               varchar(255)     not null,
-    sender_address       varchar(255)     not null,
-    receiver             varchar(255)     not null,
-    receiver_address     varchar(255)     not null,
-    receiver_coordinates geography,
-    weight               double precision not null,
-    delivery_id          bigint constraint fk8u1il8kpkdit8glq10h206h9s references deliveries,
-    created_at           timestamp(6),
-    updated_at           timestamp(6),
-    created_by           varchar(255),
-    updated_by           varchar(255)
+    id                  serial primary key,
+    created_at          timestamp(6),
+    created_by          varchar(255),
+    updated_at          timestamp(6),
+    updated_by          varchar(255),
+    deposit_address     varchar(255) not null,
+    deposit_coordinates geography    not null,
+    max_autonomy_km     integer      not null,
+    max_capacity_kg     integer      not null,
+    model_name          varchar(255) not null
 );
