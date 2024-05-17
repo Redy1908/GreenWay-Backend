aae211d5e2d0c589881a265becec9a9a6c95bf76
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-05-17 11:07:58 +0200

    Updated elevation query

--- GreenWay/src/main/java/dev/redy1908/greenway/dem/domain/DemRepository.java
+++ GreenWay/src/main/java/dev/redy1908/greenway/dem/domain/DemRepository.java
@@ -14,12 +14,14 @@ public class DemRepository {
     private EntityManager em;
 
     public List<Double> findValuesForLineString(String lineString) {
-        Query query = em.createNativeQuery("SELECT ST_Value(rast, (dp).geom) As val " +
-                        "FROM ( " +
-                        "  SELECT (ST_DumpPoints(ST_GeomFromText(:lineString, 4326))) as dp " +
-                        ") as points, dem " +
-                        "WHERE ST_Intersects(rast, (dp).geom)" +
-                        "ORDER BY (dp).path[1];")
+        Query query = em.createNativeQuery("""
+                        SELECT COALESCE(ST_Value(rast, (dp).geom), ST_NearestValue(rast, (dp).geom)) As val
+                            FROM (
+                              SELECT (ST_DumpPoints(ST_GeomFromText(:lineString, 4326))) as dp
+                            ) as points, dem
+                            WHERE ST_Intersects(rast, (dp).geom)
+                            ORDER BY (dp).path[1];
+                        """)
                 .setParameter("lineString", lineString);
 
         return query.getResultList();
