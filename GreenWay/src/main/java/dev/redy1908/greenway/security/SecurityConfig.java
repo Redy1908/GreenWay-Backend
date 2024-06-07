760dbf3782ff65c9918340ccdc14744265c8772a
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-06-07 09:58:37 +0200

    Updated

--- GreenWay/src/main/java/dev/redy1908/greenway/security/SecurityConfig.java
+++ GreenWay/src/main/java/dev/redy1908/greenway/security/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -79,12 +78,14 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
